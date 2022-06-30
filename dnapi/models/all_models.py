4d8a78f34af95fda21f806140f55e397f314d694
Yedil·Serzhan <yedilserzhan@gmail.com>
2022-06-30 02:16:07 +0200

    added new schmas for questions and choices for checking the asnwers

--- dnapi/models/all_models.py
+++ dnapi/models/all_models.py
@@ -3,35 +3,43 @@ from flask_login import UserMixin
 from marshmallow_sqlalchemy import auto_field
 from marshmallow_sqlalchemy.fields import Nested
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
+
 
 class Level(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), unique=True, nullable=False)
     level = db.relationship('Chapter', backref=db.backref('level', lazy=True))
 
+
 class Chapter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     total_score = db.Column(db.Integer, nullable=False)
     level_id = db.Column(db.Integer, db.ForeignKey('level.id'), nullable=False)
 
+
 class ChapterSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Chapter
+
     id = auto_field()
     level_id = auto_field()
 
+
 chapter_schema = ChapterSchema()
 chapters_schema = ChapterSchema(many=True)
+
 
 class Choice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(), nullable=False)
     correctness = db.Column(db.Boolean(), nullable=False)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+
 
 class ChoiceSchema(ma.SQLAlchemySchema):
     class Meta:
@@ -41,6 +49,16 @@ class ChoiceSchema(ma.SQLAlchemySchema):
 
 choice_schema = ChoiceSchema()
 choices_schema = ChoiceSchema(many=True)
+
+class ChoicewithanwersSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Choice
+    id = auto_field()
+    content = auto_field()
+    correctness = auto_field()
+
+choicewithanswers_schema = ChoicewithanwersSchema()
+choiceswithanswers_schema = ChoicewithanwersSchema(many=True)
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -55,9 +73,11 @@ class Question(db.Model):
     chapter = db.relationship('Chapter', backref=db.backref('questions', lazy=True))
     choices = db.relationship('Choice', backref=db.backref('Question', lazy=True))
 
+
 class QuestionSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Question
+
     id = auto_field()
     title = auto_field()
     description = auto_field()
@@ -67,8 +87,29 @@ class QuestionSchema(ma.SQLAlchemySchema):
     point = auto_field()
     choices = Nested(ChoiceSchema, many=True)
 
+
 question_schema = QuestionSchema()
 questions_schema = QuestionSchema(many=True)
+
+
+class QuestionwithanswersSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Question
+    id = auto_field()
+    title = auto_field()
+    description = auto_field()
+    type = auto_field()
+    hint = auto_field()
+    image_url = auto_field()
+    point = auto_field()
+    explanation = auto_field()
+    chapter_id = auto_field()
+    choices = Nested(ChoicewithanwersSchema, many=True)
+
+
+questionwithanswers_schema = QuestionwithanswersSchema()
+questionswithanswers_schema = QuestionwithanswersSchema(many=True)
+
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -76,6 +117,7 @@ class Answer(db.Model):
     user = db.relationship('User', backref=db.backref('answers', lazy=True))
     choice_id = db.Column(db.Integer, db.ForeignKey('choice.id'), nullable=False)
     choice = db.relationship('Choice', backref=db.backref('answers', lazy=True))
+
 
 class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True)
