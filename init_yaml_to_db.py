fbe8d395d48a0e439b9190c276ce449cb81f8567
Yedil·Serzhan <yedilserzhan@gmail.com>
2022-07-21 01:45:35 +0200

    add yaml to csv for normal questions and choices

--- init_yaml_to_db.py
+++ init_yaml_to_db.py
@@ -1,26 +1,67 @@
 import yaml
 
+keys = ['title',
+        'description',
+        'type',
+        'hint',
+        'explanation',
+        'image_url',
+        'point',
+        'chapter_id',
+        # 'choices',
+        # 'correct',
+        ]
+
+q_id = 1
+
+
+def parse_normal_question(q):
+    global q_id
+    tmp = [str(q_id)]
+    q_id += 1
+    for k in keys:
+        v = q[k]
+        if v:
+            v = str(v)
+            if ',' in v:
+                v = '"' + v + '"'
+            tmp.append(v)
+        else:
+            tmp.append("")
+
+    tmp = ",".join(tmp)
+    return tmp.strip()
+
+
+def parse_normal_choices(q):
+    global q_id
+    cur_id = str(q_id - 1)
+    s = []
+    for c in q['choices']:
+        tmp = []
+        for _, v in c.items():
+            tmp.append(str(v))
+        tmp.append(cur_id)
+        tmp = ",".join(tmp)
+        s.append(tmp.strip())
+    return s
+
 
 def show_json(file_path):
     with open(file_path, "r") as yaml_doc:
         yaml_to_dict = yaml.load(yaml_doc, Loader=yaml.FullLoader)
     q_csv = []
+    c_csv = []
     for q in yaml_to_dict['questions']:
-        tmp = []
-        for k, v in q.items():
-            if k not in ['choices', 'correct']:
-                if v:
-                    v = str(v)
-                    if ',' in v:
-                        v = '"' + v + '"'
-                    tmp.append(v)
-                else:
-                    tmp.append("")
+        if q['type'] in ['choose one', 'choose many']:
+            q_csv.append(parse_normal_question(q))
+            c_csv.extend(parse_normal_choices(q))
+    for q in q_csv:
+        print(q)
+    for c in c_csv:
+        print(c)
 
-        tmp = ",".join(tmp)
-        q_csv.append(tmp.strip())
-    for s in q_csv:
-        print(s, "\n")
+
 
 
 if __name__ == '__main__':
