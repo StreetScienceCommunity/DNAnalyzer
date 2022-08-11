35e7a37fb892df6fce6e77c11763d4d87390651c
Yedil·Serzhan <yedilserzhan@gmail.com>
2022-08-11 02:38:56 +0200

    add check markers for done chapters

--- routes/main.py
+++ routes/main.py
@@ -12,7 +12,7 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        user = User.query.filter_by(username=username).first()
+        user = Users.query.filter_by(username=username).first()
         if user and check_password_hash(user.password, password):
             login_user(user, remember=True)
             return redirect(url_for('index'))
@@ -27,7 +27,7 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password')
         repeat_password = request.form.get('repeat_password')
-        user = User.query.filter_by(username=username).first()
+        user = Users.query.filter_by(username=username).first()
         if user:
             flash('username already exists', category='warning')
         elif len(username) < 3 or len(username) > 20:
@@ -37,7 +37,7 @@ def register():
         elif password != repeat_password:
             flash('Passwords are not identical', category='warning')
         else:
-            new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+            new_user = Users(username=username, password=generate_password_hash(password, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash('Account created', category='success')
@@ -56,8 +56,7 @@ def logout():
 
 @bp.route('/level1/intro')
 def intro():
-    chapters = Chapter.query.all()
-    chapters = chapters_schema.dump(chapters)
+    chapters = left_chapter_menu_helper()
     return render_template("games/intro.html", chapters=chapters)
 
 
@@ -69,13 +68,8 @@ def chapter(chapter_id):
     @param chapter_id: the chapter id for showing related quiz questions
     @return: the template for quiz
     """
-    questions = Question.query.filter_by(chapter_id=chapter_id).order_by(Question.id).all()
-    questions = questions_schema.dump(questions)
-    chapter = Chapter.query.get(chapter_id)
-    chapter = chapter_schema.dump(chapter)
-    chapters = Chapter.query.all()
-    chapters = chapters_schema.dump(chapters)
-    return render_template("games/chapter.html", questions=questions, q_len=len(questions), chapter=chapter,
+    chapter_dump, chapters, questions_dump = left_chapter_menu_helper(chapter_id)
+    return render_template("games/chapter.html", questions=questions_dump, chapter=chapter_dump,
                            chapters=chapters)
 
 
@@ -88,24 +82,21 @@ def quiz_submit(chapter_id):
     @return: show result
     """
     form = request.form
-    questions = Question.query.filter_by(chapter_id=chapter_id).order_by(Question.id).all()
-    questions_dump = questionswithanswers_schema.dump(questions)
-    chapter = Chapter.query.get(chapter_id)
-    chapter_dump = chapter_schema.dump(chapter)
-    chapters = Chapter.query.all()
-    chapters = chapters_schema.dump(chapters)
+    chapter_dump, chapters, questions_dump = left_chapter_menu_helper(chapter_id)
 
     # check choice submitted if right wrong or miss
     cur_score = 0
+    db.engine.execute("delete from answer where answer.choice_id in  ( select answer.choice_id from answer, choice, chapter, users, question where answer.choice_id = choice.id and choice.question_id = question.id and question.chapter_id = chapter.id and chapter.id = %s and users.id = %s )" % (chapter_id, current_user.id))
+    db.engine.execute("delete from score where chapter_id = %s" % (chapter_id) )
     for question in questions_dump:
         submitted_answers = set(form.getlist(question['id']))
-        # for aws_id in submitted_answers:
-        #     new_answer = Answer(
-        #         user_id=current_user.id,
-        #         choice_id=aws_id
-        #     )
-        #     db.session.add(new_answer)
-        #     db.session.commit()
+        for aws_id in submitted_answers:
+            new_answer = Answer(
+                user_id=current_user.id,
+                choice_id=aws_id
+            )
+            db.session.add(new_answer)
+            db.session.commit()
         selected_correct = 0
         missed_wrong = 0
         if not form.get(question['id']):
@@ -131,5 +122,32 @@ def quiz_submit(chapter_id):
         else:
             correct_sum = selected_correct + missed_wrong
             question['score'] = question['point'] * 0 if correct_sum == 0 else len(question['choices']) / correct_sum
-    return render_template("games/quiz_result.html", questions=questions_dump, q_len=len(questions),
+    new_score = Score(
+        score = cur_score,
+        user_id=current_user.id,
+        chapter_id = chapter_id
+    )
+    db.session.add(new_score)
+    db.session.commit()
+    return render_template("games/quiz_result.html", questions=questions_dump,
                            chapter=chapter_dump, score=cur_score, chapters=chapters)
+
+
+def left_chapter_menu_helper(chapter_id=0):
+    if chapter_id != 0:
+        questions = Question.query.filter_by(chapter_id=chapter_id).order_by(Question.id).all()
+        questions_dump = questionswithanswers_schema.dump(questions)
+        chapter = Chapter.query.get(chapter_id)
+        chapter_dump = chapter_schema.dump(chapter)
+    chapters = Chapter.query.all()
+    chapters = chapters_schema.dump(chapters)
+    done_chapters = db.engine.execute('select chapter_id from score where user_id = %s' % (current_user.id))
+    done_chapters_list = [row[0] for row in done_chapters]
+
+    for ch in chapters:
+        if ch['id'] in done_chapters_list:
+            ch['done'] = 1
+    if chapter_id != 0:
+        return chapter_dump, chapters, questions_dump
+    else:
+        return chapters
