725c0e850a0c0c6c8866cecc2f77a899b398c1ec
Yedil·Serzhan <yedilserzhan@gmail.com>
2023-07-19 23:21:43 +0200

    change url path in the ari-make file as well

--- bin/ari-make.sh
+++ bin/ari-make.sh
@@ -22,7 +22,8 @@ slides_md="$dir/slides.md"
 $(which python3) -m http.server 9876 &
 
 # Process the slides
-docker run --network host -v $(pwd):/slides astefanutti/decktape automatic -s 1920x1080 http://localhost:9876/$slides $pdf; \
+# Pass the slide file name as a URL parameter
+docker run --network host -v $(pwd):/slides astefanutti/decktape automatic -s 1920x1080 ttp://0.0.0.0:9876/assets/reveal.js/index.html?slides=$slides $pdf; \
 
 # Build the slides
 echo ari.sh "$pdf" "$slides_md" "$mp4"
